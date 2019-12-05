39d63e251b7e68d194db4b840fd917757f28ae7e
taichi <t1a2i3t4i5@gmail.com>
2019-12-05 04:59:28 +0000

    デバイスでログイン機能できたよ,ジャパンマップ追加したよ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  #ユーザー作成時に名前を追加するためのストロングぱためーた
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  # 新規登録後のリダイレクト先をマイページへ
+    def after_sign_in_path_for(resource)
+      if current_user
+        flash[:notice] = "ログインに成功しました" 
+        root_url  #指定したいパスに変更
+      else
+        flash[:notice] = "新規登録完了しました。次に名前を入力してください" 
+        new_profile_path  #　指定したいパスに変更
+      end
+    end
+
+  private
   
-  def hello
-    render html: "taichi"
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) # 新規登録時(sign_up時)にnameというキーのパラメーターを追加で許可する
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
+  get 'users/show'
+
+  devise_for :users
+  root 'home_pages#index'
+  get 'home_pages/index'
+  resources :users
 end
