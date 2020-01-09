2115cd7b13693cb9cfaf561ae5e48d386d20e517
二代目の俺 <t1a2i3t4i6@gmail.com>
2020-01-09 04:55:41 +0000

    再チャレンジ

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     ENV['AWS_S3_ACCESS_KEY_ID'],  # required
-    aws_secret_access_key: ENV['AWS_S3_SECRET_ACCESS_KEY'],     # required
+    aws_access_key_id:     ENV['AKIAVX5HNTR2AZUUPMK7'],  # required
+    aws_secret_access_key: ENV['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'],     # required
     region:                'ap-northeast-2',                   # optional, defaults to 'us-east-1' オハイオ＝us-east-2
     #host:                  's3.example.com',             # optional, defaults to nil
     #endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
