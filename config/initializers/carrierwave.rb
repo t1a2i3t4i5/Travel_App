96f64e44f39d27753db8a38c0f17883a8fffc137
二代目の俺 <t1a2i3t4i6@gmail.com>
2020-01-09 04:57:42 +0000

    元に戻してみた

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,15 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog-aws'
+
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV['AKIAVX5HNTR2AZUUPMK7'],  # required
-    aws_secret_access_key: ENV['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'],     # required
-    region:                'ap-northeast-2',                   # optional, defaults to 'us-east-1' オハイオ＝us-east-2
-    #host:                  's3.example.com',             # optional, defaults to nil
-    #endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+    provider: 'AWS',
+    aws_access_key_id: 'AKIAVX5HNTR2AZUUPMK7',
+    aws_secret_access_key: '9cqenbGbSIR9SZuzRMaT9FF4mTRQdZk5DVjyFW6p',
+    region: 'ap-northeast-1'
   }
-  config.fog_directory  = 'travelapp1998'                # required
-  config.fog_public     = false                                                 # optional, defaults to true
-  config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
+
+  config.fog_directory  = 'travelapp1998'
+  config.cache_storage = :fog
 end
