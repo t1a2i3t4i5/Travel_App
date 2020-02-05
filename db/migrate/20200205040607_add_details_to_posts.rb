2fe9f7deafe8c8c8c326b12ac54f9ea1794a1186
二代目の俺 <t1a2i3t4i6@gmail.com>
2020-02-05 05:55:42 +0000

    visited_atのカラム変更

--- db/migrate/20200205040607_add_details_to_posts.rb
+++ db/migrate/20200205040607_add_details_to_posts.rb
@@ -1,6 +1,6 @@
 class AddDetailsToPosts < ActiveRecord::Migration[5.1]
   def change
     add_column :posts, :place, :string
-    add_column :posts, :visited_at, :datatime
+    add_column :posts, :visited_at, :string
   end
 end
