41e4696d7447e42eb7eee6042bb625bfd63d037d
泰知 <t1a2i3t4i5@gmail.com>
2020-09-27 17:54:06 +0900

    #close #52 ページごとにwindowのタイトルを変更したい。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = "Jaravlate"
+    if page_title.empty?
+      base_title
+    else
+      "#{ page_title } | #{ base_title }"
+    end
+  end
+
   def active_tab_post
     actions = %w[index new_arrival popular]
     actions.index(action_name)
